3007548e98c51e4e43631f6ca0e7807f6d82e821
Jesse <jessecdenier@gmail.com>
2024-01-10 18:42:15 -0600

    Built rudimentary form and random string generator

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
-// Assignment code here
+//Connect references between HTML and Javascript.
+var ifLength = document.querySelector("#ifLength");
+var generateBtn = document.querySelector("#generate");
+var formBtn = document.querySelector("#form");
+var passwordText = document.querySelector("#password");
 
-function generatePassword() {
-  return "banana";
+// Declare all possible characters.
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!"#$%&()*+,-./:;<=>?@[^_`{|}~'
+
+// Create a random string of characters.
+function generatePassword(length) {
+  let result = ' ';
+  var charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
 }
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
+// Write password to the #password input.
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
+  var passwordLength = ifLength.value;
+  var password = generatePassword(passwordLength);
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//Show or hide the form.
+function showForm() {
+  if (questions.style.display === "none") {
+    questions.style.display = "block";
+  }
+  else {
+    questions.style.display = "none";
+  }
+}
+
+// Add event listener to form button
+formBtn.addEventListener("click", showForm);
