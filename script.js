38c4bae039490f985590d3ddddececc7d5c168d7
Jesse <jessecdenier@gmail.com>
2024-01-11 13:25:26 -0600

    Changed verbage of notation, and standardized quotation marks.

--- script.js
+++ script.js
@@ -1,46 +1,45 @@
-//Connect all references between HTML and Javascript.
+//Connect all references by ID between HTML and Javascript.
 function getElementById(id) {
-  return document.querySelector("#" + id);
+  return document.querySelector('#' + id);
 }
 
-// Set strings based on character selection.
+// Assign characters to corresponding strings.
 var characterSets = {
-  lowercase: "abcdefghijklmnopqrstuvwxyz",
-  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  numerical: "0123456789",
+  lowercase: 'abcdefghijklmnopqrstuvwxyz',
+  uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  numerical: '0123456789',
   special: '!"#$%&()*+,-./:;<=>?@[^_`{|}~',
 }
 
-
 // Create a random string of characters based on user inputs.
 function generatePassword(length) {
-  var result = "";
-  var chosenCharacters = "";
+  var result = '';
+  var chosenCharacters = '';
 
   // Sets which characters to allow in the random string.
   if (ifLowercase.checked) {
     chosenCharacters += characterSets.lowercase;
   }
   else {
-    chosenCharacters = chosenCharacters.replace(characterSets.lowercase, "");
+    chosenCharacters = chosenCharacters.replace(characterSets.lowercase, '');
   }
   if (ifUppercase.checked) {
     chosenCharacters += characterSets.uppercase;
   }
   else {
-    chosenCharacters = chosenCharacters.replace(characterSets.uppercase, "");
+    chosenCharacters = chosenCharacters.replace(characterSets.uppercase, '');
   }
   if (ifNumerical.checked) {
     chosenCharacters += characterSets.numerical;
   }
   else {
-    chosenCharacters = chosenCharacters.replace(characterSets.numerical, "");
+    chosenCharacters = chosenCharacters.replace(characterSets.numerical, '');
   }
   if (ifSpecial.checked) {
     chosenCharacters += characterSets.special;
   }
   else {
-    chosenCharacters = chosenCharacters.replace(characterSets.special, "");
+    chosenCharacters = chosenCharacters.replace(characterSets.special, '');
   }
 
   // Picks random characters for the string.
@@ -50,44 +49,44 @@ function generatePassword(length) {
 
   // Returns an error if user selection is incorrect.
   if (ifLength.value < 8) {
-    result = "Too Short";
+    result = 'Too Short';
   } else if (ifLength > 128) {
-    result = "Too Long";
-  } else if (result === "") {
-    result = "Selection Necessary";
+    result = 'Too Long';
+  } else if (result === '') {
+    result = 'Selection Necessary';
   }
 
   return result;
 }
 
-// Write password to the #password input.
+// Write password to the #passwordText HTML element.
 function writePassword() {
   var passwordLength = ifLength.value;
   var password = generatePassword(passwordLength);
   passwordText.value = password;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Add event listener to generate button.
+generateBtn.addEventListener('click', writePassword);
 
 //Show or hide the form.
 function showForm() {
-  if (questions.style.display === "none") {
-    questions.style.display = "block";
+  if (questions.style.display === 'none') {
+    questions.style.display = 'block';
   }
   else {
-    questions.style.display = "none";
+    questions.style.display = 'none';
   }
 }
 
-// Add event listener to form button
-formBtn.addEventListener("click", showForm);
+// Add event listener to form button.
+formBtn.addEventListener('click', showForm);
 
-/* Copy password into clipboard */
+// Copy password into user clipboard.
 function copyPassword() {
   navigator.clipboard.writeText
     (passwordText.value);
 }
 
-// Add event listener to copy button
-copyBtn.addEventListener("click", copyPassword);
+// Add event listener to copy button.
+copyBtn.addEventListener('click', copyPassword);
